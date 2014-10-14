9ddc640dc41731e3c2fb4b5d40b9b1e453da18f7
James Lance <james@thelances.net>
2014-10-13 23:40:04 -0600

    mostly checking in for safe keeping

--- www/js/main.js
+++ www/js/main.js
@@ -1,21 +1,69 @@
 (function() {
     "use strict";
 
+    var window_ratio = $(window).width() / $(window).height();
+    window_ratio = (window_ratio > 1.4) ? 'wide' : 'normal';
+
     var photos = [];
-    var staging_photos = [];
-    var photos_staged = false;
+    var time_to_shuffle = 1 * 60 * 1000;
+    var refresh_album_time   = 15 * 60 * 1000;
+
+    var build_row = function(row, photos) {
+        // A row can have a number of different configurations:
+        // if wide then a minumum of 3, and a maximum of 10
+        // if normal then a minimum of 2 and a maximum of 8
+        var columns = (window_ratio === 'wide') ? _.random(3,5) : _.random(2,4);
+        return row;
+    };
+
+    var shuffle_row = function(row, photos) {
+        // pick one of the columns.
+        // decide if I'm going to just fill that same column or try a different configuration
+        // animate out of view the old column(s) letting the columns collapse any space
+        // animate the new column(s) into view by adding to either the right or left of the row
+    };
+
+    var shuffle_show = function(end_time, top_row, bottom_row, photos) {
+        console.log('about to shuffle the show');
+        if (_.now() > end_time) {
+            // quittin' time.
+            stage_photos();
+        } else {
+            // pick a new photo to show
+            shuffle_row(_.random(1, [top_row, bottom_row]), photos);
+            _.delay(shuffle_show, time_to_shuffle, end_time, top_row, bottom_row, photos);
+        }
+    };
+
+    var slide_show = function(staging_photos) {
+        var top_row = [];
+        $('#top_row').empty();
+        var bottom_row = [];
+        $('#bottom_row').empty();
+        // Not sure if I should iterate through old photos and explicitly remove from DOM?
+        photos = staging_photos.slice(0);
+        // prepare stage
+        // Build up initial show
+        build_row(top_row, photos);
+        build_row(bottom_row, photos);
+
+        var start_time = _.now();
+        var end_time = start_time + (refresh_album_time);
+
+        _.delay(shuffle_show, time_to_shuffle, end_time, top_row, bottom_row, photos);
+    };
 
     var finish_staging = function(staging_photos, count) {
         if (staging_photos.length < count) {
             return false;
         } else {
-            photos_staged = true;
+            slide_show(staging_photos);
             console.log(staging_photos);
         }
     };
 
-    var stage_photos = function(staging_photos) {
-        staging_photos = [];
+    var stage_photos = function() {
+        var staging_photos = [];
         $.getJSON("slideshow.json")
         .success( function(data) {
             $.each(data.images, function(key, value) {
@@ -38,5 +86,5 @@
         });
     };
 
-    stage_photos(staging_photos);
+    stage_photos();
 })();
